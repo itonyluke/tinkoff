339249e99038b815081ca25b77615092bfdfe2dc
wanisha <wanisha@studen.21-school.ru>
2022-01-17 19:55:10 +0300

    comment out unnecessary stuff

--- real_test/ex_01/ex_01.cpp
+++ real_test/ex_01/ex_01.cpp
@@ -2,24 +2,73 @@
 
 #define SIZE 10
 
-void check_int(int &i)
-{
-	if (i >= 0 && i <= 100)
-		exit(EXIT_FAILURE);
-}
+//void check_int(int &i)
+//{
+//	if (i >= 0 && i <= 100)
+//		exit(EXIT_FAILURE);
+//}
+//
+//void output_int_array(unsigned int (&arr)[SIZE])
+//{
+//	for (unsigned int i = 0; i < SIZE; i++)
+//		std::cout << arr[i] << std::endl;
+//}
+//
+//
+//void swap(unsigned int (&arr)[SIZE], unsigned int &i)
+//{
+//	unsigned int hold;
+//
+//	hold = arr[i];
+//	arr[i] = arr[i + 1];
+//	arr[i + 1] = hold;
+//}
+//
+//void bubble_sort(unsigned int (&arr)[SIZE])
+//{
+//	unsigned int i, n;
+//
+//	n = sizeof(arr)/sizeof(arr[0]);
+//	while (n != 0)
+//	{
+//		i = 0;
+//		while (i < SIZE - 1)
+//		{
+//			if (arr[i] < arr[i + 1])
+//				swap(arr, i);
+//			i++;
+//		}
+//		n--;
+//	}
+//}
+//
+//void equalize(unsigned int (&arr)[SIZE], unsigned int &average)
+//{
+//	unsigned int i = 0, j = 0, n = 0;
+//
+//	n = sizeof(arr)/sizeof(arr[0]);
+//	while (n != 0)
+//	{
+//		i = 0;
+//		while (i < SIZE)
+//		{
+//			bubble_sort(arr);
+//			while (arr[i] < average && arr[0] > average)
+//			{
+//				arr[i]++;
+//				arr[j]--;
+//			}
+//			i++;
+//		}
+//		n--;
+//	}
+//}
 
 void initialize_array_variables_to_zero(unsigned int (&arr)[SIZE])
 {
 	for (unsigned int i = 0; i < SIZE; i++)
 		arr[i] = 0;
 }
-
-void output_int_array(unsigned int (&arr)[SIZE])
-{
-	for (unsigned int i = 0; i < SIZE; i++)
-		std::cout << arr[i] << std::endl;
-}
-
 unsigned int sum_up_int_array(unsigned int (&arr)[SIZE])
 {
 	unsigned int s = 0, res = 0;
@@ -41,56 +90,6 @@ unsigned int sum_up_int_array(unsigned int (&arr)[SIZE])
 		exit(EXIT_FAILURE);
 	}
 }
-
-void swap(unsigned int (&arr)[SIZE], unsigned int &i)
-{
-	unsigned int hold;
-
-	hold = arr[i];
-	arr[i] = arr[i + 1];
-	arr[i + 1] = hold;
-}
-
-void bubble_sort(unsigned int (&arr)[SIZE])
-{
-	unsigned int i, n;
-
-	n = sizeof(arr)/sizeof(arr[0]);
-	while (n != 0)
-	{
-		i = 0;
-		while (i < SIZE - 1)
-		{
-			if (arr[i] < arr[i + 1])
-				swap(arr, i);
-			i++;
-		}
-		n--;
-	}
-}
-
-void equalize(unsigned int (&arr)[SIZE], unsigned int &average)
-{
-	unsigned int i = 0, j = 0, n = 0;
-
-	n = sizeof(arr)/sizeof(arr[0]);
-	while (n != 0)
-	{
-		i = 0;
-		while (i < SIZE)
-		{
-			bubble_sort(arr);
-			while (arr[i] < average && arr[0] > average)
-			{
-				arr[i]++;
-				arr[j]--;
-			}
-			i++;
-		}
-		n--;
-	}
-}
-
 
 int main(void)
 {
